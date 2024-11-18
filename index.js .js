bc2a0f9dbdbad7611368b534224aac52842b27ba
MrBarker47 <jebarker7@gmail.com>
2024-11-18 14:11:38 -0500

    Update files

--- index.js .js
+++ index.js .js
@@ -6,16 +6,24 @@ const port = 3000;
 
 
 
-
-
-
-
 // template engine
 function renderHello() {
     let template = document.getElementById('template').innerHTML;
     let rendered = Mustache.render(template, { name: 'Luke' });
     document.getElementById('target').innerHTML = rendered;
   }
+
+
+//Middleware
+let cookiePaser = require("cookie-paser");
+app.use(cookiePaser());
+
+
+//Error Handling
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({error: err.message});
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);

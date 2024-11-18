c0da93dc417c81166bb360eac2c7d2b209f0e9a3
MrBarker47 <jebarker7@gmail.com>
2024-11-18 11:24:24 -0500

    Update files, added mustache for a template engine

--- index.js .js
+++ index.js .js
@@ -1,4 +1,8 @@
 // Express 
 const express = require("express");
-const app = express;
+const app = express();
 const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});

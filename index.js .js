a7106e7fe427607c316cd262185e1635f8e58f58
MrBarker47 <jebarker7@gmail.com>
2024-11-18 11:48:51 -0500

    Update files

--- index.js .js
+++ index.js .js
@@ -3,6 +3,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+
+
+
+
+
+
+// template engine
+function renderHello() {
+    let template = document.getElementById('template').innerHTML;
+    let rendered = Mustache.render(template, { name: 'Luke' });
+    document.getElementById('target').innerHTML = rendered;
+  }
+
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
 });
